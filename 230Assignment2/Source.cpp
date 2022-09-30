ff9a42253d73f649989edeb35822569a9f96da42
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-09-30 16:26:08 -0700

    first attempt (sprites are loaded)

--- 230Assignment2/Source.cpp
+++ 230Assignment2/Source.cpp
@@ -8,10 +8,16 @@
 #include <SFML/Main.hpp>
 // ************************************
 using namespace sf;
+using namespace std;
+#include <string>
+#include <iostream>
 
 //global variables
-Texture tileTexture;
+
 Sprite tileSprite;
+const int spriteCount = 21;
+Texture tileTexture[spriteCount];
+Sprite spriteArr[spriteCount];
 
 //prototypes
 void handleInput(RenderWindow& window, Event& e);
@@ -28,12 +34,26 @@ int main()
     shape.setFillColor(sf::Color::Green);
 
     //Sprite
+    /*
     if (!tileTexture.loadFromFile("Assets/sprites/Platformer-0.png")) {
         //file didn't load properly
         return -1;
     }
     tileSprite.setTexture(tileTexture);
     tileSprite.setPosition(365, 530);
+    */
+    
+    for (int i = 0; i < spriteCount; i++) {
+        string tempFileName = ("Assets/sprites/Platformer-" + to_string(i) + ".png");
+        cout << tempFileName << endl;
+        if (!tileTexture[i].loadFromFile(tempFileName)) {
+            //if the sprite is loaded incorrectly
+            return -1;
+        }
+        tileSprite.setTexture(tileTexture[i]);
+        spriteArr[i] = tileSprite;     
+    }
+    
 
     //copy paste into every project (it's used for quitting)
     while (window.isOpen())
@@ -63,14 +83,19 @@ void handleInput(RenderWindow& window, Event& e) {
 }
 
 void update(RenderWindow& window) {
-
+    spriteArr[1].setPosition(Mouse::getPosition().x - 600, Mouse::getPosition().y - 310);
 }
 
+/*
+    Whenever you click with the mouse, pushback a vector with the current sprite
+        make sure to draw both the current sprite on cursor AND each of the values in the vector
+*/
 void render(RenderWindow& window) {
     //used for updating the window
     window.clear();
 
-    window.draw(tileSprite);
+    window.draw(spriteArr[1]);
+    window.draw(spriteArr[2]);
 
     window.display();
 
